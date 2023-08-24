1c2d3f962577eae3c74c329d68acf0e9a9a776a0
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-08-24 14:03:51 +0500

    Update viewquiz.blade.php

--- resources/views/admin/quizzes/viewquiz.blade.php
+++ resources/views/admin/quizzes/viewquiz.blade.php
@@ -23,77 +23,64 @@
                     <div class="card-header flex-wrap py-5">
                         <div class="card-title">
                             <h3 class="card-label">
-                                Update Questions of : {{ $data->name }}
+                                View Quiz : {{ $data->name }}
                             </h3>
                         </div>
                     </div>
                     <div class="card-body">
-                        <form id="addquestionform" enctype="multipart/form-data" method="POST" action="{{ url('admin/quizzes/addquestion') }}">
-                            @csrf
-                            <input type="hidden" value="{{ $data->id }}" name="quiz_id">
-                            <input type="hidden" id="type" value="saveandanotherquestion" name="type">
-                            <div class="row">
-                                @foreach(DB::table('questions')->where('quiz_id' , $data->id)->get() as $r) 
-                                <div class="col-md-12 mb-5">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h4>Question {{ DB::table('questions')->where('quiz_id' , $data->id)->count()+1 }}</h4>
+
+                        <table class="table table-separate table-head-custom table-checkable" id="userTable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Question Title</th>
+                                    <th scope="col">Answers</th>
+                                    <th scope="col">Correct Answer</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach(DB::table('questions')->where('quiz_id' , $data->id)->orderby('id' , 'asc')->get() as $r)
+                                <tr class="align-items-center">
+                                    <td class="sorting_1 dtr-control">
+                                        {{ $r->question }}
+                                    </td>
+                                    <td>{{ DB::table('answers')->where('question_id' , $r->id)->count() }}</td>
+                                    <td>{{ DB::table('answers')->where('id' , $r->answer_id)->first()->answer }}</td>
+                                    <td nowrap="">
+                                        <a href="javascript:void(0)" class="btn btn-sm btn-clean btn-icon" title="Edit details">
+                                             <i class="la la-edit"></i> 
+                                        </a>
+                                        <a data-toggle="modal" data-target="#deleteModal{{ $r->id }}" href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">
+                                            <i class="la la-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <div class="modal fade" id="deleteModal{{ $r->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <form method="POST" action="{{ url('admin/quizzes/delete') }}">
+                                    @csrf
+                                <input type="hidden" value="{{ $r->id }}" name="id">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Are you Sure you want to Delete this?</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <i aria-hidden="true" class="ki ki-close"></i>
+                                            </button>
                                         </div>
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <div class="col-xl-12">
-                                                    <div class="form-group">
-                                                        <label class="font-size-h6 font-weight-bolder text-dark">Question Title</label>
-                                                        <input type="text" required class="form-control form-control-solid h-auto p-5 border-0 rounded-lg font-size-h6" name="question" placeholder="Eneter Question" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div id="prescription-area">
-                                                <div class="row .w-row">
-                                                    <div class="col-xl-10">
-                                                        <div class="form-group">
-                                                            <input required placeholder="Option 1" type="text" class="form-control form-control-solid h-auto p-5 border-0 rounded-lg font-size-h6" name="option[]" placeholder=""  />
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-xl-2">
-                                                        <label class="checkbox checkbox-lg">
-                                                            <input type="radio" value="1" name="answer"/>
-                                                            <span></span> &nbsp
-                                                            Answer
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>                                            
-                                            <div class="row">
-                                                <div class="col-md-8 text-right"></div>
-                                                <div class="col-md-2 text-right">
-                                                    <span id="remove-prescription" class="btn btn-primary form-control">
-                                                        Remove Last
-                                                    </span>
-                                                </div>
-                                                <div class="col-md-2 text-right">
-                                                    <span id="add-prescription" class="btn btn-primary form-control">
-                                                        Add Option
-                                                    </span>
-                                                </div>
-                                            </div>
+                                        <div class="modal-body">
+                                            Are you Sure you want to Delete this Quiz If you Delete this Quiz then Automaticaly Deleted All Data Against This Quiz in User Panel Also?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal">Cancel</button>
+                                            <button type="submit" class="btn btn-danger font-weight-bold">Yes, Delete it</button>
                                         </div>
                                     </div>
                                 </div>
-                                @endforeach
-                                <div class="col-md-3 mt-3">
-                                    <div class="form-group">
-                                        <button type="submit" style="display: none;" id="submitbutton"></button>
-                                        <span id="saveandanotherquestion" class="btn btn-primary form-control">Save and Add Another Question</span>
-                                    </div>
-                                </div>
-                                <div class="col-md-3 mt-3">
-                                    <div class="form-group">
-                                        <span id="saveandpublishquiz" class="btn btn-primary form-control">Save and Publish Quiz</span>
-                                    </div>
-                                </div>
+                                </form>
                             </div>
-                        </form>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <!--end::Card-->
@@ -103,26 +90,4 @@
         <!--end::Entry-->
     </div>
 </div>
-<script type="text/javascript">
-    $(document).ready(function() {
-    $('#add-prescription').click(function(){
-        var intId = $("#prescription-area .w-row").length + 2 || 1;
-        var presFields = $('<div class="row w-row"> <div class="col-xl-10"> <div class="form-group"> <input placeholder="Option '+intId+'" name="option[]" type="text" required class="form-control form-control-solid h-auto p-5 border-0 rounded-lg font-size-h6" placeholder=""  /> </div> </div> <div class="col-xl-2"> <label class="checkbox checkbox-lg"> <input type="radio" value="'+intId+'" name="answer"/> <span></span> &nbsp Answer </label> </div> </div>');
-        $('#prescription-area').append(presFields);
-    });
-    $('#remove-prescription').click(function(){
-        $("#prescription-area .w-row:last").remove();
-    });
-
-    $('#saveandanotherquestion').click(function(){
-        $('#type').val('saveandanotherquestion');
-        $('#submitbutton').click();
-    });
-
-    $('#saveandpublishquiz').click(function(){
-        $('#type').val('saveandpublishquiz');
-        $('#submitbutton').click();
-    });
-});
-</script>
 @endsection
